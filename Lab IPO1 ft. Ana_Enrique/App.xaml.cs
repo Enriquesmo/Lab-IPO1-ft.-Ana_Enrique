ccfbf6c630e8de952ff45ee481d7cb12bec16379
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2022-11-14 11:44:29 +0100

    Agregar archivos de proyecto.

---
+++ Lab IPO1 ft. Ana_Enrique/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_IPO1_ft.Ana_Enrique
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
