efdf6af69e5a11eabf3d06f0b41616a06d11e6cc
AnaToledano3 <Ana.Toledano@alu.uclm.es>
2022-12-01 20:10:53 +0100

    caztar

--- Lab IPO1 ft. Ana_Enrique/ListaDeRutas.xaml.cs
+++ Lab IPO1 ft. Ana_Enrique/ListaDeRutas.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Lab_IPO1_ft.Ana_Enrique
 {
@@ -29,26 +30,28 @@ namespace Lab_IPO1_ft.Ana_Enrique
         }
         private List<Ruta> CargarContenidoXML()
         {
-            List<Pelicula> listado = new List<Pelicula>();
+            List<Ruta> listado = new List<Ruta>();
             // Cargar contenido de prueba
             XmlDocument doc = new XmlDocument();
             var fichero = Application.GetResourceStream(new Uri("Datos/peliculas.xml", UriKind.Relative));
             doc.Load(fichero.Stream);
             foreach (XmlNode node in doc.DocumentElement.ChildNodes)
             {
-                var nuevaPelicula = new Pelicula("", 0, null, "", 0);
-                nuevaPelicula.Titulo = node.Attributes["Titulo"].Value;
-                nuevaPelicula.Anio = Convert.ToInt32(node.Attributes["Anio"].Value);
-                nuevaPelicula.Duracion = Convert.ToInt32(node.Attributes["Duracion"].Value);
-                nuevaPelicula.Argumento = node.Attributes["Argumento"].Value;
-                nuevaPelicula.Caratula = new Uri(node.Attributes["Caratula"].Value, UriKind.Relative);
-                nuevaPelicula.Director = node.Attributes["Director"].Value;
-                nuevaPelicula.GeneroPelicula = node.Attributes["Genero"].Value;
-                nuevaPelicula.AltaEnVideoteca = Convert.ToDateTime(node.Attributes["AltaEnVideoteca"].Value);
-                nuevaPelicula.Visualizada = Convert.ToBoolean(node.Attributes["Visualizada"].Value);
-                nuevaPelicula.URL_IMDB = new Uri(node.Attributes["URL_IMDB"].Value, UriKind.Absolute);
-                listado.Add(nuevaPelicula);
+                var nuevaRuta = new Ruta("", 0, null, "", 0);
+                nuevaRuta.Nombre = node.Attributes["Titulo"].Value;
+                nuevaRuta.Origen = Convert.ToInt32(node.Attributes["Anio"].Value);
+                nuevaRuta.Duracion = Convert.ToInt32(node.Attributes["Duracion"].Value);
+                nuevaRuta.Descripcion = node.Attributes["Argumento"].Value;
+                nuevaRuta.Imagenlista = new Uri(node.Attributes["Caratula"].Value, UriKind.Relative);
+                nuevaRuta.guia = node.Attributes["Director"].Value;
+                nuevaRuta.material = node.Attributes["Genero"].Value;
+                nuevaRuta.FechayHora = Convert.ToDateTime(node.Attributes["AltaEnVideoteca"].Value);
+                nuevaRuta.Finalizada = Convert.ToBoolean(node.Attributes["Visualizada"].Value);
+                nuevaRuta.mapa = new Uri(node.Attributes["URL_IMDB"].Value, UriKind.Absolute);
+                listado.Add(nuevaRuta);
             }
+
+            //hay que terminarlo
             return listado;
         }
     }
