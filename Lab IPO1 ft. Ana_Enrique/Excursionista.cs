b9a56cf895db2d25e5aff4099bc5ccd4859c5697
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2022-12-07 17:09:47 +0100

    Clases finalizadas 1.0

--- Lab IPO1 ft. Ana_Enrique/Excursionista.cs
+++ Lab IPO1 ft. Ana_Enrique/Excursionista.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// HECHO
+
 namespace Lab_IPO1_ft.Ana_Enrique
 {
     public class Excursionista
     {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public Uri Foto { get; set; }
+        public int Edad { get; set; }
+        public int Telefono { get; set; }
+        public List<Ruta> Rutas { get; set; }
+
+        public Excursionista(string nombre, string apellidos, int edad, int telefono)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            Telefono = telefono;
+            Edad = edad;
+
+        }
     }
 }
