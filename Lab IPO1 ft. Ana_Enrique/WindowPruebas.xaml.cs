9a8e5630433f762b963d84ed1e9746a8889bd688
AnaToledano3 <Ana.Toledano@alu.uclm.es>
2023-01-14 19:43:20 +0100

    update

---
+++ Lab IPO1 ft. Ana_Enrique/WindowPruebas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab_IPO1_ft.Ana_Enrique
+{
+    /// <summary>
+    /// Lógica de interacción para WindowPruebas.xaml
+    /// </summary>
+    public partial class WindowPruebas : Window
+    {
+        public WindowPruebas()
+        {
+            InitializeComponent();
+        }
+    }
+}
