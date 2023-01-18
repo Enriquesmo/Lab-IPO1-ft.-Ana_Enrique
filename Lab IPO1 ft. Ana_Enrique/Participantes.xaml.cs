8f24c5792c95c51e37fb9cbcbd9ba9b2f327e8c9
Enrique Sánchez-Migallón Ochoa <Enrique.Sanchez9@alu.uclm.es>
2023-01-19 00:50:52 +0100

    Casi terminada la ventana de Participantes

--- Lab IPO1 ft. Ana_Enrique/Participantes.xaml.cs
+++ Lab IPO1 ft. Ana_Enrique/Participantes.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -20,7 +21,8 @@ namespace Lab_IPO1_ft.Ana_Enrique
     /// </summary>
     public partial class Participantes : Window
     {
-        ObservableCollection<Excursionista> lista;
+        ObservableCollection<Excursionista> listadoParticipantesAux;
+        Ruta rutaElegida;
         /************************************************************************************************/
 
         /*Inicializacion de la ventana Participantes*/
@@ -28,12 +30,42 @@ namespace Lab_IPO1_ft.Ana_Enrique
         public Participantes(Ruta ruta, ObservableCollection<Excursionista> listadoParticipantes)
         {
             InitializeComponent();
-            lista = ruta.participantes;
-            foreach (Excursionista ex in lista)
+            listadoParticipantesAux = listadoParticipantes;
+            rutaElegida = ruta;
+            for (int i = 0; i < listadoParticipantesAux.Count; i++)
             {
-                ListaParticipantes.Items.Add(ex);
+                ListaParticipantes.Items.Add(listadoParticipantesAux[i]);
             }
+
+            // FALTA HACER QUE SE PONGA EN TICK TODOS LOS QUE ESTEN AÑADIDOS
+
+            /*
+            for(int i = 0; i < ListaParticipantes.Items.Count; i++)
+            {
+                if (ruta.participantes.Contains(listadoParticipantesAux[i]))
+                    {
+                       var button = (Button)FindName("BtnAlLadoDeLaPersona");
+                       cambiarImagenDelBoton(button,1);
+                    }
+            }*/
+            /*Aqui comprobamos los excursionistas que se encuntran ya en la ruta para poder marcarlos con un check*/
+            /*
+            foreach (var item in ListaParticipantes.Items)
+            {
+                ListBoxItem itemContainer = (ListBoxItem)ListaParticipantes.ItemContainerGenerator.ContainerFromItem(item);
+                if (itemContainer != null)
+                {
+                    Button button = (Button)itemContainer.FindName("BtnAlLadoDeLaPersona");
+                    if (ruta.participantes.Contains(item))
+                    {
+                        cambiarImagenDelBoton(button, 1);
+                    }
+                }
+
+            }*/
+
             Foto.Visibility = Visibility.Hidden;
+            estadoBotones(1);
 
         }
 
@@ -41,49 +73,131 @@ namespace Lab_IPO1_ft.Ana_Enrique
 
         /*Botones de la propia ventana Participantes*/
 
-        private void ListaParticipantes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListaParticipantes_SelectionChanged(object sender, SelectionChangedEventArgs e) // Terminado
         {
-            /*
+            
             ListaParticipantes.Items.Refresh();
-            selec = ListaParticipantes.SelectedItem as Excursionista;
-            if (selec != null)
+            Excursionista seleccionado = ListaParticipantes.SelectedItem as Excursionista;
+            if (seleccionado != null)
             {
-                txbNombre.Text = selec.Nombre;
-                txbApp.Text = selec.Apellidos;
-                txbEdad.Text = selec.Edad.ToString();
-                txbTelef.Text = selec.Telefono.ToString();
-                txbDni.Text = selec.DNI;
+                txbNombre.Text = seleccionado.Nombre;
+                txbApellidos.Text = seleccionado.Apellidos;
+                txbEdad.Text = seleccionado.Edad.ToString();
+                txbTelefono.Text = seleccionado.Telefono.ToString();
+                txbDni.Text = seleccionado.DNI;
+                Foto.Visibility = Visibility.Visible;
+                if (seleccionado.Foto != null)
+                {
+                    BitmapImage imagen = new BitmapImage(seleccionado.Foto);
+                    Foto.Source = imagen;
+                }
+                else
+                {
+                    Foto.Source = null;
+                }
+                estadoBotones(2);
             }
-            */
+            
         }
         private void btnAyuda_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Aquí hay que introducir la ayuda que se facilitará al usuario para esta ventana.", "Ayuda", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
-        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        private void btnBorrar_Click(object sender, RoutedEventArgs e) // Terminado
         {
-           /*
-            ListaParticipantes.Items.Remove(ListaParticipantes.SelectedItem);
-            txbNombre.Text = "";
-            txbApp.Text = "";
-            txbEdad.Text = "";
-            txbTelef.Text = "";
-            txbDni.Text = "";
-            */
+            Excursionista excursionista = ListaParticipantes.SelectedItem as Excursionista;
+            System.Windows.MessageBoxResult result = MessageBox.Show("¿Está seguro que quiere borrar el guia?.", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.OK)
+            {
+                ListaParticipantes.Items.Remove(excursionista);
+                txbNombre.Text = "";
+                txbApellidos.Text = "";
+                txbEdad.Text = "";
+                txbTelefono.Text = "";
+                txbDni.Text = "";
+                Foto.Source = null;
+                ListBoxRutasExcursionista.Items.Clear();
+            }
+                /*
+                 ListaParticipantes.Items.Remove(ListaParticipantes.SelectedItem);
+                 txbNombre.Text = "";
+                 txbApp.Text = "";
+                 txbEdad.Text = "";
+                 txbTelef.Text = "";
+                 txbDni.Text = "";
+                 */
 
 
-        }
-
-        private void btnAnadir_Click(object sender, RoutedEventArgs e)
+            }
+        private void btnAnadir_Click(object sender, RoutedEventArgs e) // Terminado
         {
+            Excursionista excursionita = new Excursionista("", "", 0, 0, "");
+            bool esNumeroTelefono = esNumero(txbTelefono, "el Telefono");
+            bool esNumeroEdad = esNumero(txbEdad, "la Edad");
+            String nombreAux = introducirString(txbNombre.Text);
+
+            if (nombreAux.Equals(""))
+            {
+                MessageBox.Show("Debe introducir al menos\n - El nombre del excursionista.\n para Finalizar este proceso.", "Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                if (esNumeroTelefono == true && esNumeroEdad == true)
+                {
+                    System.Windows.MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres añadir al excursionista?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.OK)
+                    {
+                        excursionita.Nombre = introducirString(txbNombre.Text);
+                        excursionita.Apellidos = introducirString(txbApellidos.Text);
+                        excursionita.Edad = introducirNumero(txbEdad);
+                        excursionita.Telefono = introducirNumero(txbTelefono);
+                        excursionita.Foto = Imagen;
+                        excursionita.DNI = introducirString(txbDni.Text);
+                        ListaParticipantes.Items.Add(excursionita);
+                        ListaParticipantes.Items.Refresh();
+                        // Lo añadimos de la lista de guias principal
+                        listadoParticipantesAux.Add(excursionita);
+                    }
+                }
+            }
             /*
             Excursionista nuevo = new Excursionista(txbNombre.Text, txbApp.Text, int.Parse(txbEdad.Text), int.Parse(txbTelef.Text), txbDni.Text);
             ListaParticipantes.Items.Add(nuevo);
             */
         }
-        private void btnMod_Click(object sender, RoutedEventArgs e)
+        private void btnMod_Click(object sender, RoutedEventArgs e) // Terminado
         {
+            Excursionista excursionistaAModificar = ListaParticipantes.SelectedItem as Excursionista;
+            bool esNumeroTelefono = esNumero(txbTelefono, "el Telefono");
+            bool esNumeroEdad = esNumero(txbEdad, "la Edad");
+            String nombreAux = introducirString(txbNombre.Text);
+
+            if (nombreAux.Equals(""))
+            {
+                MessageBox.Show("Debe introducir al menos\n - El nombre del excursionista.\n para Finalizar este proceso.", "Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                if (esNumeroTelefono == true && esNumeroEdad == true)
+                {
+                    System.Windows.MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres modificar al excursionista?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                    if (result == MessageBoxResult.OK)
+                    {
+
+                        excursionistaAModificar.Nombre = introducirString(txbNombre.Text);
+                        excursionistaAModificar.Apellidos = introducirString(txbApellidos.Text);
+                        excursionistaAModificar.Edad = introducirNumero(txbEdad);
+                        excursionistaAModificar.Telefono = introducirNumero(txbTelefono);
+                        excursionistaAModificar.Foto = Imagen;
+                        excursionistaAModificar.DNI = introducirString(txbDni.Text);
+                        ListaParticipantes.Items.Refresh();
+                        // Lo eliminamos de la lista de guias principal y lo añadimos de nuevo
+                        listadoParticipantesAux.Remove(excursionistaAModificar);
+                        listadoParticipantesAux.Add(excursionistaAModificar);
+                    }
+                }
+            }
             /*
             Excursionista nuevo = new Excursionista(txbNombre.Text, txbApp.Text, int.Parse(txbEdad.Text), int.Parse(txbTelef.Text), txbDni.Text);
             selec = ListaParticipantes.SelectedItem as Excursionista;
@@ -95,19 +209,74 @@ namespace Lab_IPO1_ft.Ana_Enrique
             MessageBox.Show("Participante modificado");
             */
         }
-        private void btnActualizarFoto_Click(object sender, RoutedEventArgs e)
+        private void btnX_Click(object sender, RoutedEventArgs e) // Terminado
         {
-
+            ListaParticipantes.SelectedItem = null;
+            Foto.Source = null;
+            txbNombre.Text = "";
+            txbApellidos.Text = "";
+            txbEdad.Text = ""; ;
+            txbTelefono.Text = "";
+            txbDni.Text = "";
+            ListBoxRutasExcursionista.Items.Clear();
+            estadoBotones(1);
         }
-        private void btnX_Click(object sender, RoutedEventArgs e)
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e) // Terminado
         {
-
+            System.Windows.MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres limpiar todos los campos?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.OK)
+            {
+                Foto.Source = null;
+                txbNombre.Text = "";
+                txbApellidos.Text = "";
+                txbEdad.Text = ""; ;
+                txbTelefono.Text = "";
+                txbDni.Text = "";
+                ListBoxRutasExcursionista.Items.Clear();
+            }
         }
         private void ButtonAlLadoDeLaPersona(object sender, RoutedEventArgs e)
         {
-
+            var button = sender as Button;
+            int valor = 0;
+            Excursionista excursionista = ListaParticipantes.SelectedItem as Excursionista;
+            if(excursionista != null)
+            {
+                if (button.Content == null)
+                {
+                    valor = 2;
+                    rutaElegida.participantes.Remove(excursionista);
+                }
+                else if (button.Content.Equals("check"))
+                {
+                    valor = 2;
+                    rutaElegida.participantes.Remove(excursionista);
+                }
+                else
+                {
+                    valor = 1;
+                    rutaElegida.participantes.Add(excursionista);
+                }
+                cambiarImagenDelBoton(button, valor);
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar el excursionista primero y luego darle su respectivo boton.", "Error", MessageBoxButton.OK);
+            }
+            
         }
+        private void btnActualizarFoto_Click(object sender, RoutedEventArgs e) // Terminado
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri NuevaFoto = new Uri(openFileDialog.FileName);
+                BitmapImage nuevaImagen = new BitmapImage(new Uri(openFileDialog.FileName));
+                Foto.Source = nuevaImagen;
+                Imagen = NuevaFoto;
+            }
+        }
         // Esta variable se usa para guardar la imagen tras pulsar el boton de actualizar foto, para luego implementarla cuando se cree o modifique un guia
         public Uri Imagen { get; set; }
 
@@ -115,6 +284,32 @@ namespace Lab_IPO1_ft.Ana_Enrique
 
         /*Metodos Auxiliares para todos los botones*/
 
+        private void cambiarImagenDelBoton(Button button, int opcion) // Terminado
+        {
+            if (opcion == 1)
+            {
+                System.Windows.MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres añadir este participante a la ruta?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                if (result == MessageBoxResult.OK)
+                {
+                    ImageBrush imagenCheck = new ImageBrush();
+                    imagenCheck.ImageSource = new BitmapImage(new Uri("check.png", UriKind.RelativeOrAbsolute));
+                    button.Background = imagenCheck;
+                    button.Content = "check";
+                }
+               
+            }
+            else if(opcion == 2)
+            {
+                System.Windows.MessageBoxResult result = MessageBox.Show("¿Estás seguro de que quieres eliminar este participante a la ruta?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                if (result == MessageBoxResult.OK)
+                {
+                    ImageBrush imagenCross = new ImageBrush();
+                    imagenCross.ImageSource = new BitmapImage(new Uri("cross.png", UriKind.RelativeOrAbsolute));
+                    button.Background = imagenCross;
+                    button.Content = "cross";
+                }
+            }
+        }
         private void introducirEnLaListBox(ListBox listbox, List<string> listaAIntroducir)
         {
             if (listaAIntroducir != null)
@@ -125,7 +320,7 @@ namespace Lab_IPO1_ft.Ana_Enrique
                 }
             }
         }
-        private void estadoBotones(int opcion) 
+        private void estadoBotones(int opcion) // Terminado
         {
             if (opcion == 1)
             {
@@ -141,7 +336,7 @@ namespace Lab_IPO1_ft.Ana_Enrique
             }
             btnLimpiar.IsEnabled = true;
         }
-        private bool esNumero(TextBox textbox, string mensaje) 
+        private bool esNumero(TextBox textbox, string mensaje) // Terminado
         {
             int aDevolver;
             bool esNumero = true;
@@ -156,7 +351,7 @@ namespace Lab_IPO1_ft.Ana_Enrique
             }
             return esNumero;
         }
-        private int introducirNumero(TextBox textbox) 
+        private int introducirNumero(TextBox textbox) // Terminado
         {
             int numero = 0;
             bool siMaxPart = int.TryParse(textbox.Text, out numero);
@@ -166,7 +361,7 @@ namespace Lab_IPO1_ft.Ana_Enrique
             }
             return numero;
         }
-        private string introducirString(string emisor) 
+        private string introducirString(string emisor) // Terminado
         {
             string contenidoAMeter;
             if (emisor.Equals(null))
